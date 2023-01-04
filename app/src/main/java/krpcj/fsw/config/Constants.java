631bd0a5a3527ed7e1111a7e1de244b2b02f66d1
Stephen Flores <sf314@icloud.com>
2023-01-03 23:12:37 -0600

    feat: Define constants in separate file

---
+++ app/src/main/java/krpcj/fsw/config/Constants.java
@@ -0,0 +1,8 @@
+package krpcj.fsw.config;
+
+public class Constants {
+    public static String CLIENT_NAME = "KRPCJ Client";
+    public static String SERVER_IP = "10.0.1.202";
+    public static int SERVER_RPC_PORT = 1000;
+    public static int SERVER_STREAM_PORT = 1001;
+}
