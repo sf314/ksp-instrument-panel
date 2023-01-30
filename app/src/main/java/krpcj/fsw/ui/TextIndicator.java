dbde4861ef7ee5ec45a56462cf995edaa778b055
Stephen Flores <sf314@icloud.com>
2023-01-29 23:09:39 -0600

    feat: Create new generic text indicator for unimplemented gauges

---
+++ app/src/main/java/krpcj/fsw/ui/TextIndicator.java
@@ -0,0 +1,29 @@
+package krpcj.fsw.ui;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TextIndicator extends JPanel {
+    private double value;
+    private String name;
+
+    private JLabel valueDisplay;
+
+    public TextIndicator(String name) {
+        super();
+        this.value = 0;
+        this.valueDisplay = new JLabel();
+        valueDisplay.setText(name);
+
+        // Setup display of value
+        this.add(valueDisplay);
+
+        this.setVisible(true);
+    }
+
+    public void updateValue(double newValue) {
+        this.value = newValue;
+        String dispString = String.format("%s: %.2f", this.name, this.value);
+        this.valueDisplay.setText(dispString);
+    }
+}
