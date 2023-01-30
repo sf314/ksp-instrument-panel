0212c0c642b93862051b1d11218d94c48c9162f3
Stephen Flores <sf314@icloud.com>
2023-01-29 22:36:20 -0600

    chore: Update constants according to currently declared values

--- app/src/main/java/krpcj/fsw/config/Constants.java
+++ app/src/main/java/krpcj/fsw/config/Constants.java
@@ -2,7 +2,7 @@ package krpcj.fsw.config;
 
 public class Constants {
     public static String CLIENT_NAME = "KRPCJ Client";
-    public static String SERVER_IP = "10.0.1.202";
-    public static int SERVER_RPC_PORT = 1000;
-    public static int SERVER_STREAM_PORT = 1001;
+    public static String SERVER_IP = "10.0.1.205";
+    public static int SERVER_RPC_PORT = 50002;
+    public static int SERVER_STREAM_PORT = 50003;
 }
