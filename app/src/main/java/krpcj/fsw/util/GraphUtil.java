51200ce3eb7686133261352ff228779cc18ac529
Stephen Flores <sf314@icloud.com>
2023-01-31 23:22:58 -0600

    chore: Add javadoc to polar/cartesian conversion

--- app/src/main/java/krpcj/fsw/util/GraphUtil.java
+++ app/src/main/java/krpcj/fsw/util/GraphUtil.java
@@ -5,6 +5,13 @@ import krpcj.fsw.ui.graph.PolarPoint;
 
 public class GraphUtil {
 
+    /**
+     * Convert the given polar coordinate into cartesian coordinates. Note that
+     * it is assumed that 0deg is to the right, and 90deg is up. Thus, the
+     * corresponding cartesian y-axis points up as well.
+     * @param polar
+     * @return
+     */
     public static CartPoint polarToCart(PolarPoint polar) {
         int x = (int)(polar.r * Math.cos(polar.theta));
         int y = (int)(polar.r * Math.sin(polar.theta));
